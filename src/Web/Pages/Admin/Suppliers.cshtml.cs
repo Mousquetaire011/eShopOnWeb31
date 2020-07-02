7aac6f78270cde10eb25aade61962ea3aafa5ae1
Bazeun <gourdon.stanislas@laposte.net>
2020-07-02 15:22:55 +0200

    ajout liste fournisseur

--- src/Web/Pages/Admin/Suppliers.cshtml.cs
+++ src/Web/Pages/Admin/Suppliers.cshtml.cs
@@ -6,15 +6,26 @@ using Microsoft.eShopWeb.Web.Interfaces;
 using Microsoft.eShopWeb.Web.ViewModels;
 using System;
 using System.Threading.Tasks;
+using Microsoft.eShopWeb.Web.Services;
 
 namespace Microsoft.eShopWeb.Web.Pages.Admin
 {
     [Authorize(Roles = AuthorizationConstants.Roles.ADMINISTRATORS)]
     public class SuppliersModel : PageModel
     {
-        public void OnGet()
-        {
 
+        private readonly ICatalogViewModelService _catalogViewModelService;
+
+        public SuppliersModel(ICatalogViewModelService catalogViewModelService)
+        {
+            _catalogViewModelService = catalogViewModelService;
+        }
+
+       public CatalogIndexViewModel CatalogModel { get; set; } = new CatalogIndexViewModel();
+
+        public async Task OnGet(CatalogIndexViewModel catalogModel, int? pageId)
+        {
+            CatalogModel = await _catalogViewModelService.GetCatalogItems(pageId ?? 0, Constants.ITEMS_PER_PAGE, catalogModel.BrandFilterApplied, catalogModel.TypesFilterApplied).ConfigureAwait(true);
         }
     }
 }
