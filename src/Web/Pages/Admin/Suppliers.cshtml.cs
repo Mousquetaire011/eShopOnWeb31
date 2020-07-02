21f8e7ea320a42adb8c8f003143cb359b8813d79
alexandregab <alexandre.gabourg@imie.fr>
2020-07-02 15:01:58 +0200

    add using and change @model

--- src/Web/Pages/Admin/Suppliers.cshtml.cs
+++ src/Web/Pages/Admin/Suppliers.cshtml.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.eShopWeb.ApplicationCore.Constants;
+using Microsoft.eShopWeb.Web.Interfaces;
+using Microsoft.eShopWeb.Web.ViewModels;
+using System;
+using System.Threading.Tasks;
 
 namespace Microsoft.eShopWeb.Web.Pages.Admin
 {
+    [Authorize(Roles = AuthorizationConstants.Roles.ADMINISTRATORS)]
     public class SuppliersModel : PageModel
     {
         public void OnGet()
