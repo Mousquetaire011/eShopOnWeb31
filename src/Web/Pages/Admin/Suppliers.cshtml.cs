121f5666d356f7a9eb15eef898fe64bfc8ae66fd
laure_cambours <willwillo@hotmail.fr>
2020-07-02 10:28:30 +0200

    ajout page razor

--- src/Web/Pages/Admin/Suppliers.cshtml.cs
+++ src/Web/Pages/Admin/Suppliers.cshtml.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Microsoft.eShopWeb.Web.Controllers
+namespace Microsoft.eShopWeb.Web.Pages.Admin
 {
-    public class StockController : Controller
+    public class SuppliersModel : PageModel
     {
-        public IActionResult Index()
+        public void OnGet()
         {
-            return View();
+
         }
     }
 }
